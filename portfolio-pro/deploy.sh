eb4e02d8966170e7484d5df1a10745a8a5dc0e04
Moussashi <sy.moussa04@gmail.com>
2022-06-18 11:58:17 +0200

    WWW

--- portfolio-pro/deploy.sh
+++ portfolio-pro/deploy.sh
@@ -6,8 +6,10 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+echo 'www.moussasydev.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Moussashi/Portfolio.git master:gh-pages
+
 cd -
